0032dc541ab76c61457d6f96d07382f3147fcf76
Aneek <iamaneek.debnath@gmail.com>
2024-11-17 13:02:43 +0530

    Changes

--- Lab/C/Assignment 1/1.c
+++ Lab/C/Assignment 1/1.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-// Function to input and display an array of 10 elements
-void inputAndDisplayArray() {
+// Function to input an array of 10 elements
+void input() {
     int arr[10];
-    printf("Enter 10 elements of the array = ");
+    printf("Enter 10 elements of the array: ");
     for(int i = 0; i < 10; i++) {
         scanf("%d", &arr[i]);
     }
-    printf("The array elements are = ");
+}
+
+// Function to display the elements of the array
+void DisplayArray() {
+    int arr[10];
+    printf("The array elements are: ");
     for(int i = 0; i < 10; i++) {
         printf("%d ", arr[i]);
     }
@@ -15,13 +20,53 @@ void inputAndDisplayArray() {
 }
 
 int main() {
-    //int array1[10], array2[10];
-
     // Call the function for array1
-    inputAndDisplayArray();
+    printf("Array 1\n");
+    input();
+    DisplayArray();
 
     // Call the function for array2
-    inputAndDisplayArray();
+    printf("Array 2\n");
+    input();
+    DisplayArray();
 
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+// #include <stdio.h>
+
+// // Function to input and display an array of 10 elements
+// void inputAndDisplayArray() {
+//     int arr[10];
+//     printf("Enter 10 elements of the array = ");
+//     for(int i = 0; i < 10; i++) {
+//         scanf("%d", &arr[i]);
+//     }
+//     printf("The array elements are = ");
+//     for(int i = 0; i < 10; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+// }
+
+// int main() {
+//     //int array1[10], array2[10];
+
+//     // Call the function for array1
+//     inputAndDisplayArray();
+
+//     // Call the function for array2
+//     inputAndDisplayArray();
+
+//     return 0;
+// }
---
+++ Lab/C/Assignment 1/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+// Call the function for array2
+    // void input();
+    // void DisplayArray();
